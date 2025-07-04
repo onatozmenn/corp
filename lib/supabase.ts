ce8b83d92f0e4c075d23e3d6e73cb8f5567d725a
onatozmenn <onatozmen44@gmail.com>
2025-07-04 21:31:32 +0300

    Supabase anahtarları .env.local'a taşındı, güvenlik ve esneklik artırıldı

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://lcedyozohlcubgeiokjf.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjZWR5b3pvaGxjdWJnZWlva2pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1ODY4NzYsImV4cCI6MjA2NzE2Mjg3Nn0.58cniOlnLXirBnfwB0eSYVM8YWlAcgYQ95v8K98zFXc"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
